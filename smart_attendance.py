2a46141adfa2d68737a8c18762efd816fb1f7189
Sumanthaithal <35002990+sumanthaithal@users.noreply.github.com>
2020-09-25 11:39:17 +0530

    Add files via upload

--- smart_attendance.py
+++ smart_attendance.py
@@ -4,6 +4,7 @@ from imutils.video import VideoStream
 from pyzbar import pyzbar
 import datetime
 from imutils import face_utils
+from scipy.spatial import distance as dist
 import numpy as np
 import argparse
 import imutils
@@ -14,11 +15,15 @@ import h5py
 import pickle
 import face_recognition
 
+from firebase import firebase
+
 import os
 from PIL import Image, ImageDraw
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
 ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
 	help="path to output CSV file containing barcodes")
 args = vars(ap.parse_args())
@@ -31,6 +36,9 @@ else:
 
 with open("trained_knn_model.clf", 'rb') as f:
  	knn_clf = pickle.load(f)
+
+firebase = firebase.FirebaseApplication("https://smart-attendance-1600957380714.firebaseio.com/",None)
+
 
 # if a video path was not supplied, grab the reference to the webcam
 if not args.get("video", False):
@@ -45,6 +53,29 @@ else:
 csv = open(args["output"], "w")
 found = set()
 i=0
+
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+	# return the eye aspect ratio
+	return ear
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 3
+COUNTER = 0
+TOTAL = 0
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
 while True:	
 
@@ -62,45 +93,86 @@ while True:
 
 	barcodes = pyzbar.decode(image1)
 
+	gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+	rects = detector(gray, 0)
+	for rect in rects:
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+		# extract the left and right eye coordinates, then use the
+		# coordinates to compute the eye aspect ratio for both eyes
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+		# average the eye aspect ratio together for both eyes
+		ear = (leftEAR + rightEAR) / 2.0
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		cv2.drawContours(image1, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(image1, [rightEyeHull], -1, (0, 255, 0), 1)	
+		if ear < EYE_AR_THRESH:
+			COUNTER += 1
+		# otherwise, the eye aspect ratio is not below the blink
+		# threshold
+		else:
+			# if the eyes were closed for a sufficient number of
+			# then increment the total number of blinks
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				TOTAL += 1
+			# reset the eye frame counter
+			COUNTER = 0
+
+
 	# If no faces are found in the image, return an empty result.
-	if len(X_face_locations) != 0:
-		
-		# Find encodings for faces in the test iamge
-		faces_encodings = face_recognition.face_encodings(image, known_face_locations=X_face_locations)
+	if COUNTER != 0:
 
-		# Use the KNN model to find the best matches for the test face
-		print(np.array(faces_encodings).shape)
-		closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
-		are_matches = [closest_distances[0][i][0] <= 0.4 for i in range(len(X_face_locations))]
-
-		#draw = ImageDraw.Draw(image)
-
-		# Predict classes and remove classifications that aren't within the threshold
-		predictions = [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
-		for name, (top, right, bottom, left) in predictions:
+		if len(X_face_locations) != 0:
 			
-			cv2.rectangle(image1, (left,bottom),(right,top), (0, 255, 0), 2)
+			# Find encodings for faces in the test iamge
+			faces_encodings = face_recognition.face_encodings(image, known_face_locations=X_face_locations)
 
-			# show the face number
-			cv2.putText(image1, "{}".format(name), (left-10, top-10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+			# Use the KNN model to find the best matches for the test face
+			print(np.array(faces_encodings).shape)
+			closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+			are_matches = [closest_distances[0][i][0] <= 0.4 for i in range(len(X_face_locations))]
 
-			for barcode in barcodes:
+			#draw = ImageDraw.Draw(image)
 
-				(x, y, w, h) = barcode.rect
-				cv2.rectangle(image1, (x, y), (x + w, y + h), (0, 0, 255), 2)
- 
-				barcodeData = barcode.data.decode("utf-8")
-				barcodeType = barcode.type
+			# Predict classes and remove classifications that aren't within the threshold
+			predictions = [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+			for name, (top, right, bottom, left) in predictions:
+				
+				cv2.rectangle(image1, (left,bottom),(right,top), (0, 255, 0), 2)
+
+				# show the face number
+				cv2.putText(image1, "{}".format(name), (left-10, top-10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+				for barcode in barcodes:
+
+					(x, y, w, h) = barcode.rect
+					cv2.rectangle(image1, (x, y), (x + w, y + h), (0, 0, 255), 2)
 	
-				text = "{} ({})".format(barcodeData, barcodeType)
-				cv2.putText(image1, text, (x, y - 10),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+					barcodeData = barcode.data.decode("utf-8")
+					barcodeType = barcode.type
 		
-				if barcodeData not in found:
+					text = "{} ({})".format(barcodeData, barcodeType)
+					cv2.putText(image1, text, (x, y - 10),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 					now = datetime.datetime.now()
+					data = {
+							"Date":now.strftime("%m/%d/%Y"),
+							"Time":now.strftime("%H:%M:%S"),
+							"ID":barcodeData
+						}
+					result = firebase.post('/smart-attendance-1600957380714/Data',data)
+			
 					csv.write("{},{},{}\n".format(now.strftime("%m/%d/%Y"),now.strftime("%H:%M:%S"),
 						barcodeData))
 					csv.flush()
+
 					found.add(barcodeData)
 					frame= os.path.join(folder+"/","Output"+str(i)+ ".jpg")
 					cv2.imwrite(frame,image1)
